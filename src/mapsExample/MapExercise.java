7f015f4e4075bede9a31523be64b336d0f32be0c
Georgios Papadopoulos <linkin2895@gmail.com>
2022-08-26 18:30:41 +0100

    lesson 7

--- src/mapsExample/MapExercise.java
+++ src/mapsExample/MapExercise.java
@@ -1,2 +1,31 @@
-package mapsExample;public class MapExercise {
+package mapsExample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExercise {
+
+    public static void main(String[] args){
+       Map<String,Person> mapOfPeople = new HashMap<String,Person>();
+
+        ArrayList<Person> people = new ArrayList<Person>();
+        people.add(new Person("George","george@email.com"));
+        people.add(new Person("Giota","giota@email.com"));
+        people.add(new Person("Giorgos","giorgos@email.com"));
+
+        for (Person person: people) {
+            mapOfPeople.put(person.getEmail(), person);
+        }
+
+        System.out.println(mapOfPeople.get("giota@email.com"));
+        System.out.println(mapOfPeople.containsKey("giorgos@email.com"));
+
+
+
+
+
+
+
+    }
 }
--- src/mapsExample/Person.java
+++ src/mapsExample/Person.java
@@ -1,2 +1,44 @@
-package mapsExample;public class Person {
+package mapsExample;
+
+public class Person {
+    private String name,email;
+
+
+
+    public Person(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString(){
+    return name + " " + email;
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
